bf9880911e0a183d996a7ec17ff155a24c2cbf9e
Arthur-Amaral-cloud <arthur.asilva@sptech.school>
2025-10-13 00:36:25 -0300

    fix: Arrumando o Script do Banco

--- init.sh
+++ init.sh
@@ -502,6 +502,43 @@ CREATE USER 'logan'@'%' IDENTIFIED BY 'senha-segura123';
 GRANT INSERT,SELECT,UPDATE,DELETE ON HealthGuard.* TO 'logan'@'%';
 FLUSH PRIVILEGES;
 
+use HealthGuard;
+
+INSERT INTO MedicoesDisponiveis (nomeDaMedicao,unidadeDeMedida) VALUES
+('Porcentagem de uso da CPU','%'),
+('Frequência de uso da CPU','GHz'),
+('Uso da Memória RAM','%'),
+('Memória RAM Total','GB'),
+('Uso do disco','GB'),
+('Espaço restante do disco','GB'),
+('Espaço do Disco','GB');
+
+-- Inserções na tabela UnidadeDeAtendimento
+INSERT INTO UnidadeDeAtendimento (razaoSocial, nomeFantasia, cnpj, unidadeGestora)
+VALUES ('Hospital Vida Ltda', 'Hospital Vida', '12345678000195', 'Secretaria da Saúde');
+
+INSERT INTO UnidadeDeAtendimento (razaoSocial, nomeFantasia, cnpj, unidadeGestora)
+VALUES ('Clínica Bem Estar SA', 'Clínica Bem Estar', '98765432000177', 'Secretaria da Saúde');
+
+-- Inserções na tabela CodigoConfiguracao
+INSERT INTO CodigoConfiguracao (fkUnidadeDeAtendimento, codigo, dataExpiracao, statusCodigo)
+VALUES (1, 'ABC123DEF456GHI78901', '2025-12-31 23:59:59', 'Pedente');
+
+INSERT INTO CodigoConfiguracao (fkUnidadeDeAtendimento, codigo, dataExpiracao, statusCodigo)
+VALUES (2, 'XYZ987LMN654OPQ32102', '2025-10-31 23:59:59', 'Aceito');
+
+INSERT INTO Dac (fkUnidadeDeAtendimento,codigoValidacao,nomeDeIdentificacao) VALUES
+(1,"ABC123DEF456GHI78901","Arthur Machine");
+
+INSERT INTO MedicoesSelecionadas (fkUnidadeDeAtendimento,fkDac,fkMedicoesDisponiveis) VALUES
+(1,1,1),
+(1,1,2),
+(1,1,3),
+(1,1,4),
+(1,1,5),
+(1,1,6),
+(1,1,7);
+
 EOF
 
     # Executando o script SQL no container
